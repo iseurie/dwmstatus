86c5251adc3a094177b7128c9da56eae58cb3fef
Aлыа <iseurie@gmail.com>
2017-08-27 13:53:47 -0400

    remove superfluous addition

--- src/stat/time.rs
+++ src/stat/time.rs
@@ -14,7 +14,7 @@ impl super::Status for Time {
         format!("{:03}{:02}::{:02}/{:02}/{:02}::{:02}:{:02}:{:02}",
                 offset.num_hours() - 1, offset.num_minutes() % 60,
                 abbv_year, now.tm_mon, now.tm_mday,
-                now.tm_hour + 1, now.tm_min, now.tm_sec
+                now.tm_hour, now.tm_min, now.tm_sec
         )
     }
 }
