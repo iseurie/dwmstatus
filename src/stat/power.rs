119f06b9ab6ac6d2aba47366f9bec632331e587b
Aлыа <iseurie@gmail.com>
2017-10-01 13:45:17 -0400

    add battery time estimate

--- src/stat/power.rs
+++ src/stat/power.rs
@@ -1,41 +1,52 @@
-use ::std::io::prelude::*;
 use ::std::path::{Path, PathBuf};
 use ::std::str::{self, FromStr};
 use ::std::fs::File;
 use ::std::io::Read;
+use ::std::fmt::Debug;
 
-static PATH_BAT: &'static str = "/sys/class/power_supply/BAT0/";
-
+static BAT_PATH: &'static str = "/sys/class/power_supply/BAT0/";
 pub struct Power;
+
+fn read_bat_prop<T: FromStr> (name: &str, buf: &mut [u8]) -> T where
+        T: FromStr,
+        <T as FromStr>::Err: Debug {
+    let mut pb = PathBuf::from(BAT_PATH);
+    pb.push(name);
+    let errmsg = format!("cannot read BAT0 property `{}`", name);
+    let mut f = File::open(pb.as_path()).expect(&errmsg);
+    let flen = f.read(buf).expect(&errmsg);
+    let ret = unsafe {
+        T::from_str(
+                str::from_utf8_unchecked(&buf[..flen]).trim()
+        ).expect(&errmsg)
+    };
+    for b in buf.iter_mut().take(flen) { *b = 0; }
+    ret
+}
+
 impl super::Status for Power {
     fn head(&self) -> String {
-        let mut status_path = PathBuf::from(PATH_BAT);
-        status_path.push(Path::new("status"));
-        let mut f_status = File::open(status_path.as_path())
-            .expect("cannot open BAT0 property file");
-        let mut buf_status = [0u8];
-        f_status.read(&mut buf_status).unwrap_or_default();
-        format!("PW [{}]", buf_status[0] as char)
+        let mut buf = [0u8; 32];
+        let status: String = read_bat_prop("status", &mut buf[0..1]);
+        let mut ret = String::from("PW [") + &status[0..1];
+        (match status.as_str() {
+            "D" => {
+                let charge: usize = read_bat_prop("charge_now", &mut buf);
+                let consumption: usize = read_bat_prop("current_now", &mut buf);
+                let secs = charge as f64 / consumption as f64 * 3600f64;
+                ret + &(String::from("; ")
+                    + &(secs as u16 / 3600).to_string()
+                    + ":" + &(format!("{:02}", (secs / 60f64) as u16 % 60))
+                )
+            },
+            _ => ret
+        } + "]")
     }
 
     fn body(&self) -> String {
-        const BUF_CHARGE_LEN: u8 = 16;
-        let mut buf_charge = [0u8; BUF_CHARGE_LEN as usize];
-        let mut buf_read_charge = |prop: &str| -> usize {
-            let mut pb = PathBuf::from(PATH_BAT);
-            pb.push(prop);
-            let errmsg = format!("read BAT0 property '{}'", prop);
-            let mut charge_file = File::open(pb.as_path()).expect(&errmsg);
-            let digitc = charge_file.read(&mut buf_charge).expect(&errmsg);
-            let ret = usize::from_str(
-                    str::from_utf8(&buf_charge[..digitc])
-                        .expect(&errmsg).trim()
-            ).expect(&errmsg);
-            buf_charge = [0u8; BUF_CHARGE_LEN as usize];
-            ret
-        };
-        let charge_max = buf_read_charge("charge_full");
-        let charge_cur = buf_read_charge("charge_now");
+        let mut buf = [0u8; 16];
+        let charge_max: usize = read_bat_prop("charge_full", &mut buf);
+        let charge_cur: usize = read_bat_prop("charge_now", &mut buf);
         (charge_cur * 100 / charge_max).to_string() + "%"
     }
 }
